9a6298ef4ac6ee96c2a7f497ca0f4cc3bba212fe
salar-ghi <salar.ghi1993@gmail.com>
2025-08-09 22:06:13 +0330

    work on transforming json into code

---
+++ src/External/Presentation/JsonMappedSeedStorage/GenerateCodeService.cs
@@ -0,0 +1,27 @@
+﻿using Presentation.JsonSeedStorage;
+
+namespace Presentation.JsonMappedSeedStorage;
+
+public class GenerateCodeService
+{
+
+    private readonly IWebHostEnvironment _env;
+    public GenerateCodeService(IWebHostEnvironment env)
+    {
+        _env = env;
+    }
+
+
+
+    public async Task GenerateCodeInitializer()
+    {
+        var jsonFilePath = Path.Combine(_env.ContentRootPath, "JsonStorageFile", "Biceps_LongHeadBicep_ShortHeadBicep.json");
+        string jsonFile = await File.ReadAllTextAsync(jsonFilePath);
+        var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+        var inputExercises = JsonSerializer.Deserialize<List<WorkoutOutput>>(jsonFile, options);
+
+    }
+
+
+
+}
